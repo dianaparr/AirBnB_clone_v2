77c49077205b3c0a848f85c996828bff685041d0
Louvani <paula.louvani@gmail.com>
2021-08-10 17:25:20 -0500

    Implement classes User, city & place

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 
 class City(BaseModel, Base):
@@ -11,3 +12,4 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
 
+    places = relationship('Place', cascade="all, delete", backref='cities')
