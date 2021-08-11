b02def9d798c689e8cdb26bc6c64ca92f580acb1
Louvani <paula.louvani@gmail.com>
2021-08-10 19:01:38 -0500

    Updating task 9

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -30,8 +30,8 @@ class DBStorage():
 
     def all(self, cls=None):
         if cls is None:
-            # [User, State, City, Amenity, Place, Review]
-            cls_list = [State, City, User]
+            #  [User, State, City, Amenity, Place, Review]
+            cls_list = [User, State, City, Place, Review]
             new_list = []
             for class_name in cls_list:
                 new_list += self.__session.query(class_name).all()
--- models/place.py
+++ models/place.py
@@ -2,11 +2,13 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship, backref
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
+
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
@@ -15,6 +17,22 @@ class Place(BaseModel, Base):
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
     price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
+
+    reviews = relationship('Review', cascade="all, delete", backref='place')
+
+    @property
+    def reviews(self):
+        """
+        getter attribute cities that returns the list of Review instances
+        with state_id equals to the current Place.id
+        """
+        from models.review import Review
+        from models import storage
+        new_list = []
+        for key, value in storage.all(Review).items():
+            if self.id == value.state_id:
+                new_list.append(value)
+        return new_list
