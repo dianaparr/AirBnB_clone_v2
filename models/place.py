a78b19c53b2f5bf87543c20904444431f0d6fe66
dianaparr <dianaparra.9611@gmail.com>
2021-08-11 15:49:57 -0500

    Some small changes

--- models/place.py
+++ models/place.py
@@ -21,13 +21,13 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024))
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
     price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float)
-    longitude = Column(Float)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
@@ -53,10 +53,16 @@ class Place(BaseModel, Base):
         @property
         def amenities(self):
             """
-            getter attribute amenities that returns the list of
-            amenity_ids
+            getter attribute cities that returns the list of Review instances
+            with state_id equals to the current Place.id
             """
-            return self.amenity_ids
+            from models.amenity import Amenity
+            from models import storage
+            new_list = []
+            for key, value in storage.all(Amenity).items():
+                if self.id == value.id:
+                    new_list.append(value)
+            return new_list
 
         @amenities.setter
         def amenities(self, value):
