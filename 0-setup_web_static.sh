e2103d7ec3c9b7660a312f48b49809bcc2d85f43
dianaparr <dianaparra.9611@gmail.com>
2021-08-25 15:30:45 -0500

    Uploading task zero, setup nginx servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Install nginx web server
+# Step 1: Install nginx: Update apt packaging system
+if [ "$(command -v nginx)" == 1 ]; then
+    sudo apt-get -y update
+    sudo apt-get -y install nginx
+fi
+# Step 2: Create directories
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+# Step 3: Create fake file HTML
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+# Step 4: Create symbolic link, If the symbolic link already exists,
+# it should be deleted and recreated every time the script is ran.
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Step 5: Create permissions
+sudo chown -R ubuntu:ubuntu /data/
+# Step 6: Adding line to configurate server
+sed -i '57 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+# Step 7: Reload/restart Nginx with nginx command:
+# To force close and restart Nginx and related processes
+sudo /etc/init.d/nginx restart
