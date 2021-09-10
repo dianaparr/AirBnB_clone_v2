86713eee87f17d9703e846306a5ff272ab7bc0f4
dianaparr <dianaparra.9611@gmail.com>
2021-09-09 21:28:53 -0500

    Update file adding method close()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,3 +63,8 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """ Function that called to the reload() method to
+        deserializing the JSON file to objects """
+        self.reload()
